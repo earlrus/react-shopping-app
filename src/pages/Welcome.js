ff2919b321ca0efc160249cde989731868f10c38
aman <basilcrouch5@gmail.com>
2026-07-05 19:53:04 +0530

    test

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,24 +1,27 @@
-import './Welcome.css'
-import { useHistory } from 'react-router-dom';
+import "./Welcome.css";
+import { useHistory } from "react-router-dom";
 
+//hello
 
-const Home=()=>{
+const Home = () => {
+  const history = useHistory();
 
-    const history=useHistory();
-
-    const welcomeHandler=()=>{
-        history.replace('/home')
-    }
-    return (
-       <section className='welcome-main'>
-            <div>
-            <h1 className="welcome">Welcome</h1>
-            <img className="img-welcome" src="https://www.addictioncenter.com/app/uploads/2020/01/online_shopping_addiction-scaled.jpeg" alt="Shop"/>
-
-        </div>
-<button onClick={welcomeHandler}>Start Your Shopping Today</button>
-       </section>
-    )
-}
+  const welcomeHandler = () => {
+    history.replace("/home");
+  };
+  return (
+    <section className="welcome-main">
+      <div>
+        <h1 className="welcome">Welcome</h1>
+        <img
+          className="img-welcome"
+          src="https://www.addictioncenter.com/app/uploads/2020/01/online_shopping_addiction-scaled.jpeg"
+          alt="Shop"
+        />
+      </div>
+      <button onClick={welcomeHandler}>Start Your Shopping Today</button>
+    </section>
+  );
+};
 
 export default Home;
